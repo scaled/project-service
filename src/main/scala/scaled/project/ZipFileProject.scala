2e03c6c4e6839c863ccaac9140a8c375197052ef
Michael Bayne <mdb@samskivert.com>
2014-05-20 10:05:22 -0700

    Improve zip file completion.
    Zip files have their own internal directory structure, and we should complete
    those like we do normal file system directories, not as one giant list of path
    blobs.

--- src/main/scala/scaled/project/ZipFileProject.scala
+++ src/main/scala/scaled/project/ZipFileProject.scala
@@ -6,6 +6,7 @@ package scaled.project
 
 import java.nio.file.Path
 import java.util.zip.ZipFile
+import scala.collection.mutable.{Map => MMap}
 import scaled._
 
 /** Exposes the contents of a zip file as a project. This is dumb like [[FileProject]], but can at
@@ -14,16 +15,58 @@ import scaled._
 class ZipFileProject (val zipPath :Path, metaSvc :MetaService) extends Project(metaSvc) {
   import scala.collection.convert.WrapAsScala._
 
-  def root = zipPath
+  // map the zip entries into a faux file system
+  abstract class Node {
+    def insert (path :List[String]) :Unit
+    def lookup (path :List[String]) :Iterable[String]
+  }
+  object FileNode extends Node {
+    def insert (path :List[String]) = throw new IllegalStateException(path.toString)
+    def lookup (path :List[String]) = if (path.isEmpty) Seq()
+                                      else throw new IllegalArgumentException(path.toString)
+  }
+  class DirNode extends Node {
+    val children = MMap[String,Node]()
+    def insert (path :List[String]) :Unit = path match {
+      case h :: t => if (t.isEmpty) children.put(h, FileNode)
+                     else children.getOrElseUpdate(h, new DirNode()).insert(t)
+      case _ => throw new IllegalArgumentException(path.toString)
+    }
+    def lookup (path :List[String]) = path match {
+      case h :: t   =>
+        if (t.isEmpty) children.keys else children.get(h) match {
+          case Some(n) => n.lookup(t)
+          case None    => Seq()
+        }
+      case _ => Seq()
+    }
+  }
+  private lazy val rootNode = {
+    val node = new DirNode()
+    new ZipFile(zipPath.toFile).entries.foreach(
+      e => if (!e.isDirectory) node.insert(splitPath(e.getName)))
+    node
+  }
 
-  private val entries = Seq() ++ new ZipFile(zipPath.toFile).entries.map(_.getName)
+  // turns foo/bar/baz into List("foo/", "bar/", "baz")
+  private def splitPath (path :String) :List[String] = {
+    val cs = path.split("/", -1)
+    (cs.dropRight(1).map(c => s"$c/") :+ cs.last).toList
+  }
+
+  def root = zipPath
+  def name = root.getFileName.toString
+
   val fileCompleter = new Completer[Store]() {
     import Completer._
     def complete (prefix :String) = {
-      val matches = entries.filter(startsWithI(prefix))
-      sortedCompletion(matches.map(new ZipEntryStore(zipPath, _)), _.name)
+      val comps = splitPath(prefix)
+      val files = rootNode.lookup(comps)
+      val matches = files.filter(startsWithI(comps.last))
+      val pathpre = (if (prefix endsWith "/") comps else comps.dropRight(1)).mkString
+      sortedCompletion(matches.map(m => ZipEntryStore(zipPath, pathpre + m)),
+                       _.asInstanceOf[ZipEntryStore].entry)
     }
+    override def pathSeparator = Some("/")
   }
-
-  def name = root.getFileName.toString
 }
