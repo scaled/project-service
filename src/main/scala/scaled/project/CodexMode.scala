774a3b222372c0fab673f03626fad98808d73fd5
Michael Bayne <mdb@samskivert.com>
2014-11-18 13:19:50 -0800

    Added codex-rename-element.
    This is barely half an ass. To properly support rename refactoring (in OO
    languages), it needs to know about constructors, method overrides, and any
    other circumstance where a name occurs in multiple places. I'm not totally sure
    I want to even try to do that in a language agnostic way... blah.

--- src/main/scala/scaled/project/CodexMode.scala
+++ src/main/scala/scaled/project/CodexMode.scala
@@ -9,7 +9,7 @@ import javafx.scene.control.Tooltip
 import scala.collection.mutable.ArrayBuffer
 import scaled._
 import scaled.major.ReadingMode
-import scaled.util.{BufferBuilder, Chars}
+import scaled.util.{BufferBuilder, Chars, Errors}
 
 /** A minor mode which provides fns for interacting with a project's Codex.
   *
@@ -63,6 +63,7 @@ class CodexMode (env :Env, major :ReadingMode) extends MinorMode(env) {
     bind("codex-describe-element",  "C-c C-d").
     bind("codex-debug-element",     "C-c S-C-d").
     bind("codex-highlight-element", "C-c C-h").
+    bind("codex-rename-element",    "C-c C-r").
     bind("codex-visit-element",     "M-.");
 
   //
@@ -129,6 +130,58 @@ class CodexMode (env :Env, major :ReadingMode) extends MinorMode(env) {
       // report the number of uses found in this buffer, and elsewhere in the project
       val count = usesMap.map((src, us) => if (src != bufSource) us.length else 0).fold(0)(_ + _)
       window.emitStatus(s"${highlights().size} occurrences in this buffer, $count in other files.")
+    })
+  }
+
+  private val renameHistory = new Ring(editor.config(EditorConfig.historySize))
+
+  class Renamer (df :Def, src :Source, offsets :Array[Int]) {
+    val store = PSpaceCodex.toStore(src)
+    lazy val buffer = wspace.openBuffer(store)
+    lazy val locs = {
+      // convert the offsets to locs and sort them in reverse order
+      // (that way when we replace them, the earlier locs don't mess up the later locs)
+      val offs = if (src == df.source) df.offset +: offsets else offsets
+      (offs map buffer.loc).sortWith(_ > _).toSeq
+    }
+
+    def validate (elM :Matcher) = locs foreach { loc =>
+      if (!buffer.line(loc).matches(elM, loc.col)) throw Errors.feedback(
+        s"$store not in sync with index @ $loc. Can't rename.")
+    }
+
+    def replace (nameL :LineV) = locs foreach { loc => buffer.replace(loc, df.name.length, nameL) }
+  }
+
+  @Fn("Renames all occurrences of an element.")
+  def codexRenameElement () {
+    onElemAt(view.point(), (elem, loc, df) => {
+      if (df.kind != Kind.FUNC && df.kind != Kind.VALUE) throw Errors.feedback(
+        "Rename only supported for methods and variables. Not types or packages.")
+
+      val renamers = project.store.usesOf(df).toMapV.map(new Renamer(df, _, _))
+      if (renamers.isEmpty) throw Errors.feedback(
+        "No uses found for element at point. Perhaps try codex-reindex-project?")
+
+      def doit (save :Boolean) {
+        val elM = Matcher.exact(df.name)
+        renamers.foreach(_.validate(elM))
+        window.mini.read("New name:", df.name, renameHistory, Completer.none).onSuccess { nm =>
+          if (nm != df.name) {
+            val nameL = Line(nm)
+            renamers.foreach(_.replace(nameL))
+            if (save) renamers.foreach(_.buffer.save())
+          }
+        }
+      }
+
+      // if there are occurrences outside the current buffer, confirm the rename
+      if (renamers.size == 1 && renamers(0).store == buffer.store) doit(false)
+      else window.mini.readYN(
+        s"'${df.name}' occurs in ${renamers.size-1} source file(s) not including this one. " +
+          "Undoing the rename will not be trivial, continue?").onSuccess { yes =>
+        if (yes) doit(true)
+      }
     })
   }
 
