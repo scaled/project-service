31c320773a425b994c9ea72055e96d740a2e3492
Michael Bayne <mdb@samskivert.com>
2014-11-20 15:20:12 -0800

    Revamped to use line tags for doc, etc. state.

--- src/main/scala/scaled/project/CodexSummaryMode.scala
+++ src/main/scala/scaled/project/CodexSummaryMode.scala
@@ -41,20 +41,23 @@ object CodexSummaryMode {
 
   sealed trait Target {
     def name :String
+    def project :Project
   }
   case class DefMembers (df :Def) extends Target {
     def name = s"${df.name}:${df.qualifier}"
+    def project = df.project.asInstanceOf[Project#CodexStore].owner
   }
   case class TopLevelMembers (store :ProjectStore) extends Target {
     def name = s"${store.name} defs"
+    def project = store.asInstanceOf[Project#CodexStore].owner
   }
 
-  def visitDef (win :Window, df :Def) :Unit = visit(win, DefMembers(df))
-  def visitTopLevel (win :Window, store :ProjectStore) :Unit = visit(win, TopLevelMembers(store))
+  def visitDef (win :Window, df :Def) = visit(win, DefMembers(df))
+  def visitTopLevel (win :Window, store :ProjectStore) = visit(win, TopLevelMembers(store))
 
   private def visit (win :Window, tgt :Target) {
     val buf = win.workspace.createBuffer(
-      tgt.name, State.inits(Mode.Hint("codex-summary", tgt)), true)
+      tgt.name, tgt.project.bufferState("codex-summary", tgt), true)
     win.focus.visit(buf)
   }
 }
@@ -64,12 +67,7 @@ object CodexSummaryMode {
 class CodexSummaryMode (env :Env, tgt :CodexSummaryMode.Target) extends ReadingMode(env) {
   import CodexSummaryMode._
 
-  val project = (tgt match {
-    case TopLevelMembers(store) => store
-    case DefMembers(df) => df.project
-  }).asInstanceOf[Project#CodexStore].owner
-  // resolve our project and store it in the buffer for our project and codex minor modes
-  buffer.state[Project]() = project
+  val project = Project(buffer)
   import project.pspace
 
   override def keymap = super.keymap.
@@ -95,146 +93,121 @@ class CodexSummaryMode (env :Env, tgt :CodexSummaryMode.Target) extends ReadingM
   }
 
   @Fn("Displays a summary of the member def at the point.")
-  def zoomIn () :Unit = act(view.point(), (i,_) => i.zoomIn())
+  def zoomIn () :Unit = act(view.point(), _.zoomIn())
 
   @Fn("Visit the def at the point.")
-  def visit () :Unit = act(view.point(), (i,_) => i.visit())
+  def visit () :Unit = act(view.point(), _.visit())
 
   @Fn("Zooms in on modules and types and visits funcs and values.")
-  def visitOrZoom () :Unit = act(view.point(), (i,_) => i.visitOrZoom())
+  def visitOrZoom () :Unit = act(view.point(), _.visitOrZoom())
 
   @Fn("Expands or contracts the documentation of the def at the point.")
-  def toggleDocs () :Unit = act(view.point(), _.toggle(_))
+  def toggleDocs () :Unit = act(view.point(), _.toggle())
 
   //
   // Implementation details
 
-  val infs = SeqBuffer[Info]() ; {
+  class Info extends Line.Tag {
+    def zoomIn () {}
+    def visit () {}
+    def visitOrZoom () {}
+    def toggle () {}
+    override def key :Any = classOf[Info]
+  }
+  private val NoInfo = new Info()
+
+  private def act (p :Loc, fn :Info => Unit) = fn(buffer.line(p).lineTag(NoInfo))
+
+  if (buffer.start == buffer.end) {
     val psvc = env.msvc.service[ProjectService]
     val docr = new DocReader()
     def add (defs :Seq[Def]) {
       // group defs by access, then within access sort them by flavor, then name
       val byAcc = defs.groupBy(_.access)
+      var wroteAcc :Access = null
       for (acc <- Access.values) {
         byAcc.get(acc) foreach { defs =>
-          if (acc != Access.PUBLIC) infs += new AccessInfo(acc)
           for (mem <- defs.sortBy(d => (d.flavor, d.name))) {
             val docf = psvc.docFormatter(mem.source.fileExt)
-            /*if (mem.exported)*/ infs += new DefInfo(mem, docf, docr, "  ")
+            if (mem.kind != Kind.SYNTHETIC) {
+              // defer the writing of the access separator until we *actually* write a def with
+              // this access level; otherwise we could write the level and then discover that all
+              // the defs in it are invisible (synthetic)
+              if (acc != Access.PUBLIC && acc != wroteAcc) {
+                buffer.append(Seq(Line(s" ${acc.toString.toLowerCase}:")))
+                buffer.split(buffer.end)
+                wroteAcc = acc
+              }
+              addDefInfo(mem, docf, docr, "  ")
+            }
           }
         }
       }
     }
     tgt match {
       case TopLevelMembers(store) => // otherwise show the top-level members
-        infs += new ProjectInfo(store)
+        addProjectInfo(store)
         add(store.topLevelDefs.toSeq)
       case DefMembers(df) => // if we have a def, show it and its members
         def addParent (df :Def) :Unit = if (df != null) {
           addParent(df.outer)
-          infs += new DefInfo(df, psvc.docFormatter(df.source.fileExt), docr, "")
+          addDefInfo(df, psvc.docFormatter(df.source.fileExt), docr, "")
+          // tack an extra blank line after the module parent
+          if (df.kind == Kind.MODULE) buffer.split(buffer.end)
         }
-        infs += new ProjectInfo(df.project)
+        addProjectInfo(df.project)
         addParent(df)
         add(df.members.toSeq)
     }
     view.point() = Loc.Zero
   }
 
-  def act (p :Loc, fn :(Info, Int) => Unit) {
-    def loop (line :Int, ii :Int, off :Int) :Unit =
-      if (ii == infs.length) fn(infs(ii-1), off)
-      else {
-        val inf = infs(ii)
-        if (line < inf.length) fn(inf, off)
-        else loop(line-inf.length, ii+1, off+inf.length)
-      }
-    loop(p.row, 0, 0)
-  }
-
-  trait Info {
-    def zoomIn () :Unit
-    def visit () :Unit
-    def visitOrZoom () :Unit
-    def toggle (off :Int) :Unit
-    def length :Int
-  }
-
-  class ProjectInfo (store :ProjectStore) extends Info {
+  private def addProjectInfo (store :ProjectStore) {
     val title = s"project ${store.name}"
     buffer.append(Seq(Line(title), Line("-" * title.length)))
     buffer.split(buffer.end)
     buffer.split(buffer.end)
-
-    def zoomIn () {} // TODO?
-    def visit () {} // TODO?
-    def visitOrZoom () {} // TODO?
-    def toggle (off :Int) {} // TODO?
-    def length = 3
   }
 
-  class AccessInfo (acc :Access) extends Info {
-    // if we're not public, append a string representation to the buffer
-    buffer.append(Seq(Line(s" ${acc.toString.toLowerCase}:")))
-    buffer.split(buffer.end)
-
-    def zoomIn () {} // noop
-    def visit () {} // noop
-    def visitOrZoom () {} // noop
-    def toggle (off :Int) {} // noop
-    def length = 1
-  }
-
-  class DefInfo (val df :Def, docf :DocFormatterPlugin, docr :DocReader, indent :String)
-      extends Info {
+  private def addDefInfo (df :Def, docf :DocFormatterPlugin, docr :DocReader, indent :String) {
     val source = df.source
 
     val doc = df.doc
     val fmt = if (doc.isPresent) docf.format(df, doc.get, docr.resolve(source, doc.get))
               else DocFormatterPlugin.NoDoc
     val summary :SeqV[LineV] = fmt.summary(indent, view.width()-1)
-    lazy val full :SeqV[LineV] = fmt.full(indent, view.width()-1)
-    var docExpanded = false
+
+    val info = new Info() {
+      lazy val full :SeqV[LineV] = fmt.full(indent, view.width()-1)
+      var docExpanded = false
+
+      override def zoomIn () = pspace.codex.summarize(window, view, df)
+      override def visit () = pspace.codex.visit(window, view, df)
+      override def visitOrZoom () = df.kind match {
+        case Kind.MODULE | Kind.TYPE if (Some(df) != tgt) => zoomIn()
+        case _ => visit()
+      }
+
+      override def toggle () = if (full.length > 0) {
+        var row = 0 ; while (buffer.line(row).lineTag(NoInfo) != this) row += 1
+        val (rows, nlines) = if (docExpanded) (full.length, summary) else (summary.length, full)
+        val start = Loc(row, 0)
+        val end = buffer.replace(start, start + (rows, 0), nlines :+ Line.Empty)
+        var loc = start ; while (loc < end) { buffer.setLineTag(loc, this) ; loc = loc.nextL }
+        docExpanded = !docExpanded
+      }
+    }
 
     val sig :Seq[LineV] = df.sig match {
       case sigO if (!sigO.isPresent) => Seq(Line(s"$indent<no sig: $df>"))
       case sigO => formatSig(sigO.get, indent)
     }
 
-    def length :Int = (if (docExpanded) full else summary).length + sig.length
-
-    def zoomIn () = pspace.codex.summarize(window, view, df)
-    def visit () = pspace.codex.visit(window, view, df)
-    def visitOrZoom () = df.kind match {
-      case Kind.MODULE | Kind.TYPE if (Some(df) != tgt) => zoomIn()
-      case _ => visit()
-    }
-
-    def toggle (offset :Int) :Unit = if (full.length > 0) {
-      val (rows, nlines) = if (docExpanded) (full.length, summary) else (summary.length, full)
-      val start = Loc(offset, 0)
-      buffer.replace(start, start + (rows, 0), nlines :+ Line.Empty)
-      docExpanded = !docExpanded
-    }
-
     // start by appending our summary and signature to the buffer
+    var loc = buffer.end
     buffer.append(summary ++ sig)
+    // tag all generated lines with our info
+    while (loc < buffer.end) { buffer.setLineTag(loc, info) ; loc = loc.nextL }
     buffer.split(buffer.end)
-
-    private def stripDoc (line :String) = {
-      // strip leading whitespace
-      val l1 = line.replaceAll("^\\s*", "")
-      // if this is Java or Scala, strip Javadoc cruft (TODO: how will we generalize this to more
-      // languages? Have the Codex provide a doc post-processor?)
-      val l2 = source.fileExt match {
-        case "java"|"scala" =>
-          l1.replaceAll("^/\\*\\s*", "").replaceAll("\\s*\\*/\\s*$", "").replaceAll("^\\*\\s*", "")
-        case _ => l1
-      }
-      if (l2.length == 0) None else Some(l2)
-    }
-
-    private def toDocLine (line :String) =
-      Line.builder(indent + line).withStyle(CodeConfig.docStyle).build()
   }
 }
