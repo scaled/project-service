6f82225977067f623943e1dc1fcba1699e82c95a
Michael Bayne <mdb@samskivert.com>
2015-10-11 21:29:16 -0700

    Use new spam build-test script.

--- bin/travis.sh
+++ bin/travis.sh
@@ -2,20 +2,14 @@
 #
 # Builds and tests (for travis-ci.org)
 
-# create and clean our temp build directory
-mkdir -p target/spam
-SPAM=`cd target/spam ; pwd`
-rm -rf $SPAM/*
-cd $SPAM
+wget https://raw.githubusercontent.com/scaled/pacman/master/bin/build-test.sh
 
-# download the spam script
-rm -f spam
-wget https://raw.githubusercontent.com/scaled/pacman/master/bin/spam
-chmod a+rx spam
+# build and test a bunch of project-related packages
+GITROOT=git:https://github.com/scaled/
+sh build-test.sh $GITROOT/project-service.git
+sh build-test.sh $GITROOT/java-project.git
+sh build-test.sh $GITROOT/scala-project.git
+sh build-test.sh $GITROOT/maven-project.git
+sh build-test.sh $GITROOT/sbt-project.git
 
-# install/build the packages
-./spam -d -Dscaled.meta=$SPAM install project-service
-./spam -d -Dscaled.meta=$SPAM install java-project
-./spam -d -Dscaled.meta=$SPAM install scala-project
-./spam -d -Dscaled.meta=$SPAM install maven-project
-./spam -d -Dscaled.meta=$SPAM install sbt-project
+rm build-test.sh
