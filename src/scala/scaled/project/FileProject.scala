783756e3da9fc9a9506476ed45537685dd80bea2
Michael Bayne <mdb@fb.com>
2016-04-19 15:20:47 -0700

    Added ignorer for path.

--- src/scala/scaled/project/FileProject.scala
+++ src/scala/scaled/project/FileProject.scala
@@ -126,6 +126,12 @@ object FileProject {
     override def toString = s"m/$regex/"
   }
 
+  /** Returns an ignorer for a specific path. */
+  def ignorePath (_path :Path) = new Ignorer {
+    override def apply (path :Path) = path == _path
+    override def toString = _path.toString
+  }
+
   /** The standard set of directories that are ignored when enumerating all project dirs. */
   val stockIgnores = Seq(dotfileIgnorer, ignoreName(".git"),
                          ignoreName(".hg"), ignoreName(".svn")) // TODO: more?
