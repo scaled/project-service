88db6209ceac50c8ce2d77bebc728e06effde443
Michael Bayne <mdb@samskivert.com>
2017-12-31 18:43:52 -0800

    Build/test the -mode projects now.

--- bin/travis.sh
+++ bin/travis.sh
@@ -9,8 +9,8 @@ wget https://raw.githubusercontent.com/scaled/pacman/master/bin/build-test.sh
 GITROOT=git:https://github.com/scaled
 sh build-test.sh \
   $GITROOT/project-service.git \
-  $GITROOT/java-project.git \
-  $GITROOT/scala-project.git \
+  $GITROOT/java-mode.git \
+  $GITROOT/scala-mode.git \
   $GITROOT/maven-project.git \
   $GITROOT/sbt-project.git
 
