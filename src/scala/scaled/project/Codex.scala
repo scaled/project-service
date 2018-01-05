6c8891d6a951026716e562afed79d5d6d72b3b96
Michael Bayne <mdb@samskivert.com>
2018-01-05 14:10:07 -0800

    Move CodexComponent and CodexStore into Codex.
    Would be nice to nest them in Codex, but that results in name conflicts.

--- src/scala/scaled/project/Codex.scala
+++ src/scala/scaled/project/Codex.scala
@@ -6,7 +6,7 @@ package scaled.project
 
 import codex.extract.{Extractor, SourceSet, TextWriter, Writer}
 import codex.model.{Def, Element, Kind, Ref, Relation, Sig, Source}
-import codex.store.{ProjectStore, Query}
+import codex.store.{ProjectStore, MapDBStore, Query}
 import java.nio.file.{Path, Files}
 import java.util.{ArrayList, Optional, LinkedHashSet, HashMap}
 import scaled._
@@ -36,6 +36,34 @@ object Codex {
     case zes :ZipEntryStore => new Source.ArchiveEntry(zes.zipFile.toString, zes.entry)
     case _ => throw new IllegalArgumentException(s"Can't convert $store to Codex Source.")
   }
+}
+
+/** Used to report Codex info inside a project. */
+class CodexComponent (codex :Codex, store :CodexStore) extends Project.Component {
+
+  override def addToBuffer (buffer :RBuffer) {
+    // while this buffer is open, keep an up to date SourceIndex in its state
+    val conn = codex.indexed.onValue { idx =>
+      if (idx.store == buffer.store) buffer.state[SourceIndex]() = idx
+    }
+    buffer.killed.onEmit { conn.close() }
+  }
+
+  /** Appends a description of this store to `bb`. */
+  override def describeSelf (bb :BufferBuilder) {
+    bb.addSubHeader("Codex:")
+    bb.add(Line.builder(s"Defs: ${store.defCount}").withLineTag(Visit.Tag(new Visit() {
+      protected def go (window :Window) = CodexSummaryMode.visitTopLevel(window, store)
+    })).build())
+  }
+}
+
+/** A [[MapDBStore]] with some extra integration with Scaled bits. */
+class CodexStore (val root :Project.Root, indexFile :Path) extends MapDBStore(
+  root.toString, indexFile) {
+
+  /** Returns true if this store has no defs. */
+  def isEmpty () = !topLevelDefs.iterator.hasNext
 }
 
 /** Manages code indexes for projects, using the Codex index service.
