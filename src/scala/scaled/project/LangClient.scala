8ed2f15728819f4792db3d410309402d9459078d
Michael Bayne <mdb@samskivert.com>
2019-02-20 14:38:22 -0800

    Pass empty InitializedParams to initialized().
    Future proof! LOL

--- src/scala/scaled/project/LangClient.scala
+++ src/scala/scaled/project/LangClient.scala
@@ -165,7 +165,7 @@ abstract class LangClient (
     trace(s"Initializing at root: $root")
     messages.emit(s"$name langserver initializing...")
     server.initialize(initParams).thenAccept(rsp => {
-      server.initialized()
+      server.initialized(new InitializedParams())
       serverCaps.succeed(rsp.getCapabilities)
       messages.emit(s"$name langserver ready.")
     }).exceptionally(err => {
